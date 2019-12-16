ada25765e0e1341fbc63154051fca716aa93024e
Sibo Wang <375973811@qq.com>
2019-12-16 22:32:20 +1300

    -

--- Answer/script.js
+++ Answer/script.js
@@ -11,11 +11,12 @@ const newTaskForm = document.querySelector('[data-new-task-form]')
 const newTaskInput = document.querySelector('[data-new-task-input]')
 const clearCompleteTasksButton = document.querySelector('[data-clear-complete-tasks-button]')
 
-const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_LIST_KEY = 'task.myLists' //此处啥意思
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
-let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let myLists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 
+//左侧清单
 listsContainer.addEventListener('click', e => {
   if (e.target.tagName.toLowerCase() === 'li') {
     selectedListId = e.target.dataset.listId
@@ -23,45 +24,54 @@ listsContainer.addEventListener('click', e => {
   }
 })
 
+//右侧清单
 tasksContainer.addEventListener('click', e => {
   if (e.target.tagName.toLowerCase() === 'input') {
-    const selectedList = lists.find(list => list.id === selectedListId)
-    const selectedTask = selectedList.tasks.find(task => task.id === e.target.id)
+    const selectedList = myLists.find(list => list.id === selectedListId) //为啥用ID
+    const selectedTask = selectedList.tasks.find(task => task.id === e.target.id) // 存在tasks属性？
     selectedTask.complete = e.target.checked
     save()
-    renderTaskCount(selectedList)
+    renderTaskCount(selectedList) //计算任务数目的
   }
 })
 
+//清理完成的任务
 clearCompleteTasksButton.addEventListener('click', e => {
-  const selectedList = lists.find(list => list.id === selectedListId)
+  const selectedList = myLists.find(list => list.id === selectedListId)
   selectedList.tasks = selectedList.tasks.filter(task => !task.complete)
   saveAndRender()
 })
 
+//删除整个list
 deleteListButton.addEventListener('click', e => {
-  lists = lists.filter(list => list.id !== selectedListId)
+  myLists = myLists.filter(list => list.id !== selectedListId)
   selectedListId = null
+  //项目围绕这个selectedListId为核心变化@@@@
   saveAndRender()
 })
 
+//添加新的list
 newListForm.addEventListener('submit', e => {
   e.preventDefault()
   const listName = newListInput.value
   if (listName == null || listName === '') return
+  //return啥都没有？？@@@@@@@@@@@
   const list = createList(listName)
   newListInput.value = null
-  lists.push(list)
+  myLists.push(list) //储存起来
   saveAndRender()
 })
 
+//添加新的任务
 newTaskForm.addEventListener('submit', e => {
   e.preventDefault()
   const taskName = newTaskInput.value
   if (taskName == null || taskName === '') return
+  //啥都不return。。。。
+  、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、、
   const task = createTask(taskName)
   newTaskInput.value = null
-  const selectedList = lists.find(list => list.id === selectedListId)
+  const selectedList = myLists.find(list => list.id === selectedListId)
   selectedList.tasks.push(task)
   saveAndRender()
 })
@@ -80,7 +90,7 @@ function saveAndRender() {
 }
 
 function save() {
-  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(myLists))
   localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
 }
 
@@ -88,7 +98,7 @@ function render() {
   clearElement(listsContainer)
   renderLists()
 
-  const selectedList = lists.find(list => list.id === selectedListId)
+  const selectedList = myLists.find(list => list.id === selectedListId)
   if (selectedListId == null) {
     listDisplayContainer.style.display = 'none'
   } else {
@@ -120,7 +130,7 @@ function renderTaskCount(selectedList) {
 }
 
 function renderLists() {
-  lists.forEach(list => {
+  myLists.forEach(list => {
     const listElement = document.createElement('li')
     listElement.dataset.listId = list.id
     listElement.classList.add("list-name")
